dcd06875f636e6a98061ba82c2d8dbe2d523700c
moussma8 <mahad.moussa-abdillahi@univ-rouen.fr>
2026-01-07 00:47:34 +0100

    Add Platform source code (remove submodule)

---
+++ Platform/Controllers/signalR.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamingPlatform.Controllers
+{
+    public class signalR : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Platform/Program.cs
@@ -0,0 +1,37 @@
+using GamingPlatform.Hubs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GamingPlatform.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<GamingPlatformContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GamingPlatformContext") ?? throw new InvalidOperationException("Connection string 'GamingPlatformContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
